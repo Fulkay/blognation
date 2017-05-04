7d89dfd5969025b452ccbb80bdaa7f6980cf6d32
Fulkay <kayiranga@yandex.com>
2017-05-04 11:39:13 +0100

    saved all changes on my blog project at 9H22 4-20-201y

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn blognation.wsgi --log-file -
--- src/blog/wsgi.py
+++ src/blog/wsgi.py
@@ -1,6 +1,6 @@
 
 """
-WSGI config for gettingstarted project.
+WSGI config for blognation project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
