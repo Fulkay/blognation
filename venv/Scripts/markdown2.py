abfc9b239a42cf7a5ee6cfdc7e7311dce225889a
Fulkay <kayiranga@yandex.com>
2017-05-05 19:34:49 +0100

    updated tcl and scripts
    Signed-off-by: Fulkay <kayiranga@yandex.com>

--- venv/Scripts/markdown2.py
+++
@@ -1,4 +0,0 @@
-#!c:\users\user\pycharmprojects\blognation\venv\scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'markdown2==2.3.4','markdown2.py'
-__requires__ = 'markdown2==2.3.4'
-__import__('pkg_resources').run_script('markdown2==2.3.4', 'markdown2.py')
